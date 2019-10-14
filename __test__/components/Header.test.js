754719e087ab3fd7182fc35fef50d6cf75ff66ae
AleksBDevCorner <aleks.bobrovs@gmail.com>
2019-10-14 07:22:04 +0300

    added tests for Header component

--- __test__/components/Header.test.js
+++ __test__/components/Header.test.js
@@ -13,4 +13,37 @@ describe('Header component tests', () => {
 
     expect(header.length).toBe(1);
   });
+
+  it('should render Logo', () => {
+    const wrapper = shallowHeaderWrapper();
+    const logo = findInWrapper(wrapper, "[data-test='main-logo']");
+
+    expect(logo.length).toBe(1);
+  });
+
+  it('should render hamburger menu icon', () => {
+    const wrapper = shallowHeaderWrapper();
+    const hamburgerMenuIcon = findInWrapper(
+      wrapper,
+      "[data-test='hamburger-menu-icon']"
+    );
+
+    expect(hamburgerMenuIcon.length).toBe(1);
+  });
+
+  it('should render with empty background-color property', () => {
+    const wrapper = shallowHeaderWrapper();
+
+    expect(
+      wrapper
+        .find('JSXStyle')
+        .children()
+        .text()
+    ).toEqual(expect.stringContaining(`background-color:;`));
+  });
+
+  it('should pass default header background state to Logo props', () => {
+    const wrapper = shallowHeaderWrapper();
+    const logo = findInWrapper(wrapper, "[data-test='main-logo']");
+  });
 });
